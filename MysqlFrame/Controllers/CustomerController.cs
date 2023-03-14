1c6fe2a95f685d71d1649e546c52a8c9c30ae97f
Akhil1404 <akhilaparadhi6236@gmail.com>
2023-03-14 21:42:53 +0530

    I

---
+++ MysqlFrame/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MysqlFrame.Controllers
+{
+    public class CustomerController : Controller
+    {
+        MySqlConnection conn = new MySqlConnection("server=localhost;user id=root;database=mysqldb");
+        // GET: Customer
+        public ActionResult Index()
+        {
+            MySqlDataAdapter adp = new MySqlDataAdapter("SELECT * FROM `customer`", conn);
+
+            DataTable dt = new DataTable();
+
+            adp.Fill(dt);
+
+            return View(dt);
+        }
+    }
+}
